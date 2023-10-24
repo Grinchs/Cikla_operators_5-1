b37fe412b5e2271cda404275fd3e96c733144dc3
Grinchs <marcisg2345@gmail.com>
2023-10-24 05:12:01 +0300

    Add project files.

---
+++ Cikla_operators_5-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ievadiet skaitli: ");
+        int n = int.Parse(Console.ReadLine());
+
+        {
+            long faktorials = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                faktorials *= i;
+                Console.WriteLine($"{i} * {faktorials} = {faktorials}");
+            }
+        }
+    }
+}
